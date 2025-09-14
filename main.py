365fca876b9062aa80e65274e82887940f64a9b7
Lakshman <lakshman1122000@gmail.com>
2025-09-14 23:43:09 +0530

    commited

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from api import git_routes
-from models.response_model import APIResponse  # your unified response model
+from models.response_model import APIResponse 
 
 app = FastAPI(title="Neo to CF Migrator")
 
-# Routers
 app.include_router(git_routes.router, prefix="/git", tags=["Git"])
 
 @app.get("/")
