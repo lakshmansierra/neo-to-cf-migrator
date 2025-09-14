6e1196e2a72dcc6829a165f195cdeb5cd4254b4f
Lakshman <lakshman1122000@gmail.com>
2025-09-15 01:15:19 +0530

    commited

--- api/git_routes.py
+++ api/git_routes.py
@@ -1,28 +1,31 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from models.repo_request import RepoRequest
-from models.response_model import APIResponse
+from fastapi.responses import JSONResponse
 from services import git_service
 from utils.response_helper import make_response
 
 router = APIRouter()
 
-@router.post("/fetch", response_model=APIResponse)
+@router.post("/fetch")
 def fetch_repo(request: RepoRequest):
     try:
         repo_path = git_service.clone_repo(
             repo_url=request.repo_url,
             pat=request.pat,
         )
-        return make_response(
-            status="success",
+        return JSONResponse(
             status_code=200,
-            message="Repository cloned successfully",
-            data={"cloned_to": repo_path}
+            content=make_response(
+                status="success",
+                status_code=200,
+                message="Repository cloned successfully",
+                data={"cloned_to": repo_path, "description": request.description}
+            ).dict()
         )
     except Exception as e:
-        raise HTTPException(
+        return JSONResponse(
             status_code=500,
-            detail=make_response(
+            content=make_response(
                 status="error",
                 status_code=500,
                 message=f"Failed to fetch repo: {str(e)}"
