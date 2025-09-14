5461b1a298d6f1226e45cd69b5481c74f8be6535
Lakshman <lakshman1122000@gmail.com>
2025-09-14 23:42:42 +0530

    commited

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from api import git_routes, migration_routes
+from api import git_routes
 from models.response_model import APIResponse  # your unified response model
 
 app = FastAPI(title="Neo to CF Migrator")
